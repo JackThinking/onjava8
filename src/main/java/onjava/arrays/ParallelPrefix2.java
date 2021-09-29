4e5680ded71f276dd259dc060d45fa59a272d083
CitrixQian <sijieqian@foxmail.com>
2021-09-29 23:59:41 +0800

    学习数组

---
+++ src/main/java/onjava/arrays/ParallelPrefix2.java
@@ -0,0 +1,20 @@
+package onjava.arrays;
+
+import onjava.Rand;
+
+import java.util.Arrays;
+
+import static onjava.ArrayShow.show;
+
+public class ParallelPrefix2 {
+  public static void main(String[] args) {
+    String[] strings = new Rand.String(1).array(8);
+    show(strings);
+    Arrays.parallelPrefix(strings, (a, b) -> a + b);
+    show(strings);
+  }
+}
+/* Output:
+[b, t, p, e, n, p, c, c]
+[b, bt, btp, btpe, btpen, btpenp, btpenpc, btpenpcc]
+*/
