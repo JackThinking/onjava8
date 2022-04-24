34c9ce38066c009f319106e2cdf1708e3b358c3f
CitrixQian <sijieqian@foxmail.com>
2022-04-24 22:39:24 +0800

    学习设计模式

---
+++ src/main/java/onjava/patterns/TypeMap.java
@@ -0,0 +1,25 @@
+// patterns/TypeMap.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// Generic TypeMap works for any types.
+package onjava.patterns;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class TypeMap<T> {
+  public final Map<Class, List<T>> map = new HashMap<>();
+
+  public void add(T o) {
+    Class type = o.getClass();
+    map.computeIfAbsent(type, k -> new ArrayList<T>()).add(o);
+  }
+
+  public Stream<List<T>> values() {
+    return map.values().stream();
+  }
+}
---
+++ src/main/java/onjava/patterns/trash/DynaFactory.java
@@ -0,0 +1,41 @@
+// patterns/trash/DynaFactory.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// Dynamic discovery of Trash types.
+package onjava.patterns.trash;
+
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynaFactory {
+  private Map<String, Constructor> constructors = new HashMap<>();
+  private String packageName;
+
+  public DynaFactory(String packageName) {
+    this.packageName = packageName;
+  }
+
+  // 解耦了生成对象和所需的构造入参，TrashInfo 是个值对象
+  @SuppressWarnings("unchecked")
+  public <T extends Trash> T create(TrashInfo info) {
+    try {
+      String typename = "patterns." + packageName + "." + info.type;
+      return (T)
+          constructors.computeIfAbsent(typename, this::findConstructor).newInstance(info.data);
+    } catch (Exception e) {
+      throw new RuntimeException("Cannot create() Trash: " + info, e);
+    }
+  }
+
+  // 基于反射和 map 实现多态性
+  private Constructor findConstructor(String typename) {
+    try {
+      System.out.println("Loading " + typename);
+      return Class.forName(typename).getConstructor(double.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Trash(double) Constructor Not Found: " + typename, e);
+    }
+  }
+}
